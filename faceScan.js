fcadfba6ef718571f15e6935f1532a2d13f14202
Arnab Ganguly <arnab.ganguly@carenow.healthcare>
2022-07-11 18:41:44 +0530

    algo updated & build folder added

--- faceScan.js
+++ faceScan.js
@@ -1,4 +1,4 @@
-import * as faceLandmarksDetection from '@tensorflow-models/face-landmarks-detection';
+import { FaceMesh } from "@mediapipe/face_mesh";
 
 const totalCalibrationTime = 20000;
 let minimumScanTime = 60000;
@@ -63,39 +63,46 @@ const drawPath = (points) => {
   return region;
 }
 
-const getRegion = async () => {
-  const prediction = await fmesh.estimateFaces(video, { flipHorizontal: false });
-  if (prediction?.[0]?.keypoints?.length > 0) {
-    faceCircleRegion = drawPath(
-      [10, 338, 297, 332, 284, 251, 389, 356, 454, 323, 361, 288,
-        397, 365, 379, 378, 400, 377, 152, 148, 176, 149, 150, 136,
-        172, 58, 132, 93, 234, 127, 162, 21, 54, 103, 67, 109].map(
-          idx => [prediction[0].keypoints[idx].x, prediction[0].keypoints[idx].y]
-        )
-    );
-    minX = Infinity; minY = Infinity; maxX = 0; maxY = 0;
-    const points = [
-      114, 121, 120, 119, 118, 117, 111, 116, 123, 147, 187, 207, 206, 203, 142, 126, 217, // Left  Cheek
-      343, 350, 349, 348, 347, 346, 340, 345, 352, 376, 411, 427, 426, 423, 371, 355, 437, // Right Cheek
-    ].map(idx => {
-      const { x, y } = prediction[0].keypoints[idx];
-      minX = Math.min(minX, x);
-      minY = Math.min(minY, y);
-      maxX = Math.max(maxX, x);
-      maxY = Math.max(maxY, y);
-      return [x, y];
-    });
-    const region = new Path2D();
-    region.addPath(drawPath(points.slice(0, 17)));
-    region.addPath(drawPath(points.slice(17)));
-    noDetectionCount = 0;
-    isFaceInView = true;
-    return region;
-  } else {
-    isFaceInView = false;
-    return undefined;
+const getRegion = () => new Promise(async (resolve, reject) => {
+  try {
+    fmesh.send({ image: video });
+    fmesh.onResults(results => {
+      if (results?.multiFaceLandmarks?.[0]?.length > 0) {
+        faceCircleRegion = drawPath(
+          [10, 338, 297, 332, 284, 251, 389, 356, 454, 323, 361, 288,
+            397, 365, 379, 378, 400, 377, 152, 148, 176, 149, 150, 136,
+            172, 58, 132, 93, 234, 127, 162, 21, 54, 103, 67, 109].map(
+              idx => [(results.multiFaceLandmarks[0][idx].x * canvas.width), (results.multiFaceLandmarks[0][idx].y * canvas.height)]
+            )
+        );
+        minX = Infinity; minY = Infinity; maxX = 0; maxY = 0;
+        const points = [
+          114, 121, 120, 119, 118, 117, 111, 116, 123, 147, 187, 207, 206, 203, 142, 126, 217, // Left  Cheek
+          343, 350, 349, 348, 347, 346, 340, 345, 352, 376, 411, 427, 426, 423, 371, 355, 437, // Right Cheek
+        ].map(idx => {
+          const x = (results.multiFaceLandmarks[0][idx].x * canvas.width);
+          const y = (results.multiFaceLandmarks[0][idx].y * canvas.height);
+          minX = Math.min(minX, x);
+          minY = Math.min(minY, y);
+          maxX = Math.max(maxX, x);
+          maxY = Math.max(maxY, y);
+          return [x, y];
+        });
+        const region = new Path2D();
+        region.addPath(drawPath(points.slice(0, 17)));
+        region.addPath(drawPath(points.slice(17)));
+        noDetectionCount = 0;
+        isFaceInView = true;
+        resolve(region);
+      } else {
+        isFaceInView = false;
+        resolve(undefined);
+      }
+    })
+  } catch (err) {
+    reject(err);
   }
-}
+});
 
 const calcRGB_fromImageData = (imgData) => {
   let count = 0, sumRGB = { r: 0, g: 0, b: 0 };
@@ -131,7 +138,7 @@ const drawCanvas = (drawRegion = undefined, cutRegion = { region: undefined, rec
     ctx.restore();
     return calcRGB_fromImageData(bloodRegion);
   } catch (err) {
-    throw undefined;
+    throw err;
   }
 }
 
@@ -153,7 +160,7 @@ const scan = async (loop_start_time) => {
         if (noDetectionCount > 100) {
           onErrorCallback(new Error('Unable to measure your vitals.\nTry to look at the camera the next time.'));
         } else {
-          if (timeElapsed >= (totalCalibrationTime + minimumScanTime)) canStop = true;
+          if (timeElapsed > (totalCalibrationTime + minimumScanTime)) canStop = true;
           else canStop = false;
           let region = undefined;
           if ((calibrationFPSArray.reduce((sum, value) => sum + value, 0) / calibrationFPSArray.length) < 15) region = undefined;
@@ -172,6 +179,7 @@ const scan = async (loop_start_time) => {
           requestAnimationFrame(scan);
         }
       } else {
+        canStop = true;
         stopScan();
       }
     }
@@ -193,7 +201,7 @@ const startScan = async (
     isFaceInView = false;
     noDetectionCount = 0;
     faceCircleRegion = undefined;
-    minX = Infinity, minY = Infinity, maxX = 0, maxY = 0;
+    minX = Infinity; minY = Infinity; maxX = 0; maxY = 0;
     calibrationFPSArray = [];
     raw_intensity = [];
     ppg_time = [];
@@ -206,17 +214,21 @@ const startScan = async (
     minimumScanTime = minimumScanTime_inMS;
     totalScanTime = totalScanTime_inMS;
 
-    fmesh = await faceLandmarksDetection.createDetector(faceLandmarksDetection.SupportedModels.MediaPipeFaceMesh, {
-      runtime: 'mediapipe',
-      solutionPath: modelPath,
+    fmesh = new FaceMesh({ locateFile: file => `${modelPath}/${file}` });
+    fmesh.setOptions({
+      enableFaceGeometry: false,
       refineLandmarks: false,
-      maxFaces: 1
+      selfieMode: false,
+      maxNumFaces: 1,
     });
+    await fmesh.initialize();
 
     // Set up front-facing camera
     video = document.getElementById("videoInput");
-    await setupCamera();
-    video.play();
+    if (video) {
+      await setupCamera();
+      video.play();
+    } else throw new Error('Cannot get the video element.');
 
     // Create canvas and drawing context
     canvas = document.getElementById("canvasOutput");
@@ -224,7 +236,7 @@ const startScan = async (
       canvas.width = video.videoWidth;
       canvas.height = video.videoHeight;
       ctx = canvas.getContext("2d");
-    }
+    } else throw new Error('Cannot get the canvas element.');
 
     // start prediction loop
     start_time = performance.now();
@@ -236,7 +248,7 @@ const startScan = async (
   }
 }
 
-export default {
+const faceScan = {
   startScan,
   stopScan,
   onFrame: (callback = ({ type = '', timeElapsed = 0, isLightMode = false, fps = 0 }) => { }) => {
@@ -253,3 +265,5 @@ export default {
   canStop: () => canStop,
   isFaceInView: () => isFaceInView,
 };
+
+export default faceScan;
